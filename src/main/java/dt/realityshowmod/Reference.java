abc1f99dd96c0a4975522f928ddbec0fff9cd82e
SiLeNTxTrYH4Rd <sturgisbailey@gmail.com>
2017-08-06 00:33:40 -0400

    Changed Reference

--- src/main/java/dt/realityshowmod/Reference.java
+++ src/main/java/dt/realityshowmod/Reference.java
@@ -6,6 +6,6 @@ public class Reference
 	public static final String NAME = "Reality Show Mod";
 	public static final String VERSION = "0.1";
 	
-	public static final String CLIENTPROXY = "silent.realityshowmod.proxy.ClientProxy";
-	public static final String COMMONPROXY = "silent.realityshowmod.proxy.CommonProxy";
+	public static final String CLIENTPROXY = "dt.realityshowmod.proxy.ClientProxy";
+	public static final String COMMONPROXY = "dt.realityshowmod.proxy.CommonProxy";
 }
